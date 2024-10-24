7436994d5853895a59b51ca693f6a1f1e7b8af95
RohitDen <157422362+RohitDen@users.noreply.github.com>
2024-10-24 14:34:47 -0700

    Update 121.py

--- 121.py
+++ 121.py
@@ -1,65 +1,100 @@
-# INSTRUCTIONS:
-#   TODO 1:
-#   Run this code and click on the circle a few times. Document what happens, or
-#   doesn't happen. Stop the code and observe the error message. Document what 
-#   you think this means.
-#
-#   TODO 2:
-#   Run this code again and click on the square a few times. Document what 
-#   happens in the python console. Stop the code. 
-
 import turtle as trtl
 import random as rand
 
-score = 0 
+score = 0
 
-# circular turtle will not use a global variable
+# Create circular turtle (spot)
 spot = trtl.Turtle()
 spot.goto(-100, 0)
 spot.shape("circle")
 spot.shapesize(3)
 
-# square shaped turtle will use a global variable
+# Create square shaped turtle (box)
 box = trtl.Turtle()
-box.goto(100,0)
+box.goto(100, 0)
 box.shape("square")
 box.shapesize(3)
 
-new_xpos = rand.randint(-200,200)
-new_ypos = rand.randint(-150,150)
+font_setup = ("Arial", 20, "normal")
+timer = 30
+counter_interval = 1000   #1000 represents 1 second
+timer_up = False
 
-def spot_clicked(x,y):
-    change_position()
+def spot_clicked(x, y):
+    timer_up
+    if timer_up == False:
+       update_score_for_spot(x,y)
+       change_position()
+    else:
+       spot.hideturtle()
 
 
-
-spot.onclick(spot_clicked)
+def box_clicked(x, y):
+    timer_up
+    if timer_up == False:
+       update_score_for_box(x,y)
+       change_position2()
+    else:
+       box.hideturtle()
 
 def change_position():
- spot.goto(new_xpos, new_ypos)
-  
-# attempts to update the score for spot, but will not work because this function
-# does not have access to the score that was created above
-def update_score_for_spot(x,y):
-  global score
-  score +=1
-  print(score)
+    new_xpos = rand.randint(-200, 200)
+    new_ypos = rand.randint(-150, 150)
+    spot.goto(new_xpos, new_ypos)
 
-# update_score_for_box will update the score for spot
-def update_score_for_box(x,y):
-  global score # gives this function access to the score that was created above
-  score += 1
-  print(score)
-  
+def change_position2():
+    new_xpos2 = rand.randint(-200, 200)
+    new_ypos2 = rand.randint(-150, 150)
+    box.goto(new_xpos2, new_ypos2)
 
-#---------events----------
-spot.onclick(update_score_for_spot)
-box.onclick(update_score_for_box)
+#Game functions
+def countdown():
+  global timer, timer_up
+  counter.clear()
+  timer_up
+  if timer <= 0:
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+  else:
+    counter.write("Timer: " + str(timer), font=font_setup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counter_interval) 
 
-spot_clicked(new_xpos , new_ypos)
-change_position()
+# Score update functions
+def update_score_for_spot(x, y):
+    global score
+    score += 1
+    score_writer.clear()
+    score_writer.write(score, font=font_setup)
+
+def update_score_for_box(x, y):
+    global score
+    score += 1
+    score_writer.clear()
+    score_writer.write(score, font=font_setup)
+
+# Setup score display
+score_writer = trtl.Turtle()
+score_writer.penup()
+score_writer.goto(100, 200)
+score_writer.pendown()
+score_writer.hideturtle()
+
+#countdown timer
+counter =  trtl.Turtle()
+counter.penup()
+counter.goto(-100,200)
+counter.pendown()
+
+# Event bindings
+spot.onclick(spot_clicked)
+box.onclick(box_clicked)
 
 
+
+# Start the main event loop
 wn = trtl.Screen()
+wn.ontimer(countdown, counter_interval)
 wn.mainloop()
+
 
